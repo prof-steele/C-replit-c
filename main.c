dd8b4410c6765b954070d5c92e6437fecd982788
bks <bks@cs.rit.edu>
2023-01-12 14:30:46 -0500

    added main(argc,argv) skeleton code

---
+++ main.c
@@ -0,0 +1,19 @@
+//
+// TODO file documentation
+// author: TODO
+// description:
+// TODO
+//
+
+#include <stdio.h>
+
+/// TODO function documentation
+/// @param argc count of command line tokens
+/// @param argv array of cstring tokens
+/// @return status value for OS use
+
+int main( int argc, char * argv[]) {
+    
+    return 0;
+}
+
